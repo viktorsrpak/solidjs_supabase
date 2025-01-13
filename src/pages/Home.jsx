91c92b6f421a72d2bbfa019154d45e75b785964b
viktorsrpak <viktorsrpak06@gmail.com>
2025-01-13 11:10:23 +0100

    dodano

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
+import { createEffect, createSignal } from "solid-js";
 import { useAuth } from "../components/AuthProvider";
+import { supabase } from "../services/supabase";
+import { A } from "@solidjs/router";
 
 export default function Home(props) {
     const session = useAuth();
 
+    const [projects, setProjects] = createSignal(null);
+
+    createEffect(async () => {
+        if (session()) {
+            const { data, error } = await supabase
+                .from("projects")
+                .select();
+
+            if (!error) {
+                setProjects(data);
+            }
+        }
+    });
+
+
+
     return (
-        <div>
-        </div>
+        <>
+        <Show when={!session()}>
+
+        </Show>
+        </>
     )
 }
